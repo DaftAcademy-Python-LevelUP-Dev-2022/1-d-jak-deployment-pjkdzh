93e9f061165a3e6c497866c951b46811c67e3618
pjkdzh <ewelina_banach@interia.eu>
2022-05-22 12:59:44 +0200

    task2

--- tasks.py
+++ tasks.py
@@ -6,3 +6,27 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {'start': '1970-01-01'}
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
+
+@app.get("/method")
+def method_get():
+    return {"method": "GET"}
+
+@app.put("/method")
+def method_put():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def method_options():
+    return {"method": "OPTIONS"}
+
+@app.delete("/method")
+def method_delete():
+    return {"method": "DELETE"}
+
+@app.post("/method")
+def method_post():
+    return {"method": "POST"}
